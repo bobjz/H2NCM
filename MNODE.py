4640ffe03df4d0aa7eac71907b2426e07c4cc4f4
bobjz <43043287+bobjz@users.noreply.github.com>
2024-05-06 13:34:26 -0700

    Add files via upload

---
+++ MNODE.py
@@ -0,0 +1,83 @@
+import itertools
+import numpy as np
+import os
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from torchvision.ops import MLP
+from MNODE_model import *
+from utils import *
+torch.set_default_dtype(torch.float64)
+device=None
+#comment this out if not using GPU
+GPU_ID=2
+device = torch.device('cuda:'+str(GPU_ID) if torch.cuda.is_available() else 'cpu')
+print(device)
+
+
+cases=np.load("Final_T1DEXI_CASES.npy")
+ranks=np.load("Final_T1DEXI_RANKS.npy")
+
+print(cases.shape)
+print(ranks.shape)
+
+
+repeats=3
+rng=np.random.default_rng(seed=2024)
+perms=np.zeros((repeats,cases.shape[0]),dtype='int32')
+for i in range(repeats):
+    perms[i]=rng.permutation(cases.shape[0])
+
+dag=[[[0,3,4,5,6],[],[5]],\
+     [[1],[0],[2]],\
+     [[1,2],[],[2]],\
+     [[2,3],[],[2]],\
+     [[4],[2,3],[3]],\
+     [[4,5],[],[2]],\
+     [[6],[1],[2]]]
+beta=1e4
+#tune hyperparam
+for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
+    rmse=[]
+    er=[]
+    best_param_list=[]
+    for repeat in range(3):
+        for test_split in range(6):
+            num_hidden_layers=[2,3]
+            mlp_size=[16,24,32]
+            dropout=[0]
+            hyper_params=[mlp_size,num_hidden_layers,dropout]
+            list_params=np.array(list(itertools.product(*hyper_params)))
+            scores=np.zeros(list_params.shape[0])
+            for i in range(len(list_params)):
+                #tune hyperparams with cv
+                params=list_params[i]
+                for val_split in range(3):
+                    torch.manual_seed(2023)
+                    train,val,test,train_mean,train_std=cv_split(perms,cases,ranks,repeat,test_split,val_split,batch_size=64)
+                    model=MNODE_LSTM(DAG=dag,latent_size=len(dag),output_ind=0,\
+                                     mlp_size=int(params[0]),num_hidden_layers=int(params[1]),dropout=params[2])
+                    #total_params = sum(p.numel() for p in model.parameters())
+                    #print(total_params)
+                    train_h,val_h,test_h=train_model(model,alpha,beta,train,val,test,epochs=100,lr=2*1e-3,device=device)
+                    scores[i]+=np.min(val_h)/3
+
+            best_param=list_params[np.argmin(scores)]
+            print(f"best_param is {best_param}")
+            best_param_list.append(best_param)
+            torch.manual_seed(2023)
+            train,val,test,train_mean,train_std=cv_split(perms,cases,ranks,repeat,test_split,3,batch_size=64)
+            model=MNODE_LSTM(DAG=dag,latent_size=len(dag),output_ind=0,\
+                             mlp_size=int(best_param[0]),num_hidden_layers=int(best_param[1]),dropout=best_param[2])
+            train_h,val_h,test_h=train_model(model,alpha,beta,train,val,test,epochs=100,lr=2*1e-3,\
+                                             device=device,path=f"results/MNODE_{alpha}_{repeat}_{test_split}.pth")
+            print(f"repeat {repeat} test_split {test_split} pred{train_std[0]*np.sqrt(test_h[np.argmin(val_h)][0])} causal{test_h[np.argmin(val_h)][1]}")
+            rmse.append(train_std[0]*np.sqrt(test_h[np.argmin(val_h)][0]))
+            er.append(test_h[np.argmin(val_h)][1])
+
+    np.save(f"results/MNODE_a{alpha}_pred.npy",rmse)
+    np.save(f"results/MNODE_a{alpha}_causal.npy",er)
+    np.save(f"results/MNODE_a{alpha}_best_params.npy",best_param_list)
+    print(f"MNODE_{alpha}_{repeat}_{test_split} RMSE {np.mean(rmse)}")
+    print(f"MNODE_{alpha}_{repeat}_{test_split} Classification Error Rate {np.sort(er)}")
---
+++ utils.py
@@ -0,0 +1,145 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+class FormDataset(Dataset):
+    """
+    Standerd Pytoch Dataset Constructor
+    """
+    def __init__(self, cases, ranks, window=6):        
+        self.cases = cases
+        self.ranks = ranks
+        self.window = window
+
+    def __len__(self):
+        return len(self.ranks)
+
+    def __getitem__(self, idx):
+        window=self.window
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        history=self.cases[idx,:,:-window-1]
+        future_cov=self.cases[idx,:,-window-1:-1,1:]
+        starting_cgm=self.cases[idx,:,-window-1:-window,0:1]
+        output_cgm=self.cases[idx,:,-window:,0:1]
+        rankings=self.ranks[idx]
+        
+        sample = (history,starting_cgm,future_cov,\
+                  output_cgm,rankings)
+        return sample
+    
+
+def cv_split(perms,cases,ranks,rep,outer_fold,inner_fold,ts=12,vs=21,batch_size=16):
+#split and form data loarder
+    perm=perms[rep]
+    cases_s=cases[perm]
+    ranks_s=ranks[perm]
+    
+    test_cases=cases_s[outer_fold*ts:(outer_fold+1)*ts]
+    test_ranks=ranks_s[outer_fold*ts:(outer_fold+1)*ts]
+    cv_cases=np.concatenate([cases_s[0:outer_fold*ts],cases_s[(outer_fold+1)*ts:]],axis=0)
+    cv_ranks=np.concatenate([ranks_s[0:outer_fold*ts],ranks_s[(outer_fold+1)*ts:]],axis=0)
+    
+    val_cases=cv_cases[inner_fold*vs:(inner_fold+1)*vs]
+    val_ranks=cv_ranks[inner_fold*vs:(inner_fold+1)*vs]
+    train_cases=np.concatenate([cv_cases[0:inner_fold*vs],cv_cases[(inner_fold+1)*vs:]],axis=0)
+    train_ranks=np.concatenate([cv_ranks[0:inner_fold*vs],cv_ranks[(inner_fold+1)*vs:]],axis=0)
+    
+    train_mean=np.mean(train_cases[:,0],axis=(0,1))
+    train_std=np.std(train_cases[:,0],axis=(0,1))
+    train_cases=np.divide(train_cases-train_mean,train_std)
+    val_cases=np.divide(val_cases-train_mean,train_std)
+    test_cases=np.divide(test_cases-train_mean,train_std)
+    
+    train=DataLoader(FormDataset(train_cases,train_ranks),\
+                                   batch_size=batch_size)
+    val=DataLoader(FormDataset(val_cases,val_ranks),\
+                                   batch_size=len(val_ranks))
+    test=DataLoader(FormDataset(test_cases,test_ranks),\
+                                   batch_size=len(test_ranks))
+
+    return train,val,test,train_mean,train_std
+
+
+def train_model(model,alpha,beta,train,val,test,epochs,lr,device=None, verbose=False, path=None):
+    #train/validate model with train and val, save to path
+    if (device):
+        model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    loss_fn1=nn.MSELoss()
+    loss_fn2=nn.CrossEntropyLoss()
+    train_losses=[]
+    val_losses=[]
+    test_losses=[]
+    best_val=1e7
+    for epoch in range(epochs):
+        model.train()
+        train_loss1=0
+        train_loss2=0
+        for batch, (past,y0,x,y,rank) in enumerate(train):
+            if device:
+                past=past.to(device)
+                y0=y0.to(device)
+                x=x.to(device)
+                y=y.to(device)
+                rank=rank.to(device)
+            preds=[]
+            for i in range(4):
+                preds.append(model(past[:,i],y0[:,i],x[:,i]))
+            rank_bg=torch.concat([torch.mean(pred[:,:,0],dim=-1,keepdim=True) for pred in preds[1:]],dim=-1)
+            pred_rank=nn.functional.softmax(rank_bg*beta,dim=-1)
+            loss1 = loss_fn1(preds[0], y[:,0])
+            loss2 = loss_fn2(torch.log(pred_rank+1e-7),rank)
+            loss = (1-alpha)*loss1+alpha*loss2   
+            loss.backward(retain_graph=True)
+            optimizer.step()
+            optimizer.zero_grad()
+            train_loss1+=loss1.item()*len(rank)
+            train_loss2+=loss2.item()*len(rank)
+        train_size=len(train.dataset)
+        train_losses.append([train_loss1/train_size,train_loss2/train_size])
+
+        model.eval()
+        with torch.no_grad():
+
+            for batch, (past,y0,x,y,rank) in enumerate(val):
+                if device:
+                    past=past.to(device)
+                    y0=y0.to(device)
+                    x=x.to(device)
+                    y=y.to(device)
+                    rank=rank.to(device)
+                preds=[]
+                for i in range(4):
+                    preds.append(model(past[:,i],y0[:,i],x[:,i]))
+                rank_bg=torch.concat([torch.mean(pred[:,:,0],dim=-1,keepdim=True) for pred in preds[1:]],dim=-1)
+                pred_rank=nn.functional.softmax(rank_bg*beta,dim=-1)
+                loss1 = loss_fn1(preds[0], y[:,0])
+                loss2 = loss_fn2(torch.log(pred_rank+1e-7),rank)
+                loss_val = (1-alpha)*loss1+alpha*loss2  
+                valid_loss = loss_val.item()
+                val_losses.append(valid_loss)
+                if valid_loss<best_val and path:
+                    best_val=valid_loss
+                    torch.save(model.state_dict(),path)
+                if verbose:
+                    print(f"validation loss at epoch {epoch} pred {loss1.item()} causal {loss2.item()}")
+
+            for batch, (past,y0,x,y,rank) in enumerate(test):
+                if device:
+                    past=past.to(device)
+                    y0=y0.to(device)
+                    x=x.to(device)
+                    y=y.to(device)
+                    rank=rank.to(device)
+                preds=[]
+                for i in range(4):
+                    preds.append(model(past[:,i],y0[:,i],x[:,i]))
+                rank_bg=torch.concat([torch.mean(pred[:,:,0],dim=-1,keepdim=True) for pred in preds[1:]],dim=-1)
+                pred_rank=nn.functional.softmax(rank_bg*beta,dim=-1)
+                pred_rank2=nn.functional.softmax(rank_bg*1e7,dim=-1)
+                loss1 = loss_fn1(preds[0], y[:,0])
+                test_losses.append([loss1.item(),round(torch.sum(torch.abs(pred_rank2-rank)).item()/2/len(rank),3)])
+                
+    return train_losses, val_losses, test_losses
