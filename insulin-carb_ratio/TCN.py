018b1bf371ac805696f52e0b482be11632ae7095
bobjz <43043287+bobjz@users.noreply.github.com>
2024-06-03 20:41:36 -0700

    Update TCN.py

--- insulin-carb_ratio/TCN.py
+++ insulin-carb_ratio/TCN.py
@@ -15,8 +15,8 @@ device = torch.device('cuda:'+str(GPU_ID) if torch.cuda.is_available() else 'cpu
 print(device)
 
 
-cases=np.load("/dfs/scratch1/bobjz/ICML_paper_data/Final_T1DEXI_CASES.npy")
-ranks=np.load("/dfs/scratch1/bobjz/ICML_paper_data/Final_T1DEXI_RANKS.npy")
+cases=np.load("/dfs/scratch1/bobjz/ICML_paper_data/new_icml_cases.npy")
+ranks=np.load("/dfs/scratch1/bobjz/ICML_paper_data/new_icml_ranks.npy").astype("float64")
 
 print(cases.shape)
 print(ranks.shape)
@@ -35,8 +35,9 @@ for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
     rmse=[]
     er=[]
     best_param_list=[]
-    for repeat in range(3):
+    for repeat in range(1):#change to 3 for alpha=1
         for test_split in range(6):
+            seed=2023+repeat
             num_layers=[2,3]
             conv_size=[16,24,32]
             kernel_size=[2,3,4]
@@ -48,9 +49,9 @@ for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
                 #tune hyperparams with cv
                 params=list_params[i]
                 for val_split in range(3):
-                    torch.manual_seed(2023)
-                    train,val,test,train_mean,train_std=cv_split2(perms,cases,ranks,repeat,test_split,val_split,batch_size=64,\
-                                                                 train_intervention="insulin_carb", test_intervention="inscarb_ratio")
+                    torch.manual_seed(seed)
+                    train,val,test,train_mean,train_std=cv_split2(perms,cases,ranks,repeat,test_split,val_split,batch_size=72,\
+                                                                 train_intervention="insulin_carb", test_intervention="ins_carb_ratio")
                     model=TCN_wrapper(5,6, num_channels=[int(params[1])]*int(params[0]),\
                                       kernel_size=int(params[2]), dropout=params[3])
                     #total_params = sum(p.numel() for p in model.parameters())
@@ -61,9 +62,9 @@ for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
             best_param=list_params[np.argmin(scores)]
             print(f"best_param is {best_param}")
             best_param_list.append(best_param)
-            torch.manual_seed(2023)
-            train,val,test,train_mean,train_std=cv_split2(perms,cases,ranks,repeat,test_split,3,batch_size=64,\
-                                                                 train_intervention="insulin_carb", test_intervention="inscarb_ratio")
+            torch.manual_seed(seed)
+            train,val,test,train_mean,train_std=cv_split2(perms,cases,ranks,repeat,test_split,3,batch_size=72,\
+                                                                 train_intervention="insulin_carb", test_intervention="ins_carb_ratio")
             model=TCN_wrapper(5,6, num_channels=[int(best_param[1])]*int(best_param[0]),\
                                       kernel_size=int(best_param[2]), dropout=best_param[3])
             train_h,val_h,test_h=train_model(model,alpha,beta,train,val,test,epochs=100,lr=2*1e-3,\
