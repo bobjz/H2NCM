f305e9e4010f0b70296b3dac7110f7ba75b5bb3b
bobjz <43043287+bobjz@users.noreply.github.com>
2024-06-03 20:48:28 -0700

    Update LPSC.py

--- synthetic_experiments/LPSC.py
+++ synthetic_experiments/LPSC.py
@@ -31,11 +31,11 @@ for i in range(repeats):
 closure_input_sizes=[3]
 beta=1e4
 #tune hyperparam
-for alpha in [1]:
+for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
     rmse=[]
     er=[]
     best_param_list=[]
-    for repeat in range(2):
+    for repeat in range(3):
         for test_split in range(5):
             num_hidden_layers=[2,3]
             mlp_size=[16,32]
