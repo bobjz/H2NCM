39c7889ab432a62a3795d2b8ae1a4b50e0842c8b
bobjz <43043287+bobjz@users.noreply.github.com>
2024-06-03 20:54:43 -0700

    Update LPSC_reduced.py

--- LPSC_reduced.py
+++ LPSC_reduced.py
@@ -16,11 +16,8 @@ device = torch.device('cuda:'+str(GPU_ID) if torch.cuda.is_available() else 'cpu
 print(device)
 
 
-#cases=np.load("/dfs/scratch1/bobjz/ICML_paper_data/Final_T1DEXI_CASES.npy")
-#ranks=np.load("/dfs/scratch1/bobjz/ICML_paper_data/Final_T1DEXI_RANKS.npy")
-
-cases=np.load("Final_T1DEXI_CASES.npy")
-ranks=np.load("Final_T1DEXI_RANKS.npy")
+cases=np.load("/dfs/scratch1/bobjz/ICML_paper_data/new_icml_cases.npy")
+ranks=np.load("/dfs/scratch1/bobjz/ICML_paper_data/new_icml_ranks.npy").astype("float64")
 print(cases.shape)
 print(ranks.shape)
 
@@ -34,12 +31,13 @@ for i in range(repeats):
 closure_input_sizes=[2,2,2,3,2,2,2,4,2]
 beta=1e4
 #tune hyperparam
-for alpha in [0,1e-4]:
+for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
     rmse=[]
     er=[]
     best_param_list=[]
-    for repeat in range(3):
+    for repeat in range(1): #change to 3 for alpha=1
         for test_split in range(6):
+            seed=2023+repeat
             num_hidden_layers=[2,3]
             mlp_size=[16,32]
             latent_size=[8,16]
@@ -50,8 +48,8 @@ for alpha in [0,1e-4]:
                 #tune hyperparams with cv
                 params=list_params[i]
                 for val_split in range(3):
-                    torch.manual_seed(2023)
-                    train,val,test,train_mean,train_std=cv_split(perms,cases,ranks,repeat,test_split,val_split,batch_size=64)
+                    torch.manual_seed(seed)
+                    train,val,test,train_mean,train_std=cv_split(perms,cases,ranks,repeat,test_split,val_split,batch_size=72)
                     model=LPSC_LSTM(closure_input_sizes,latent_size=int(params[2]),mlp_size=int(params[1]),num_hidden_layer=int(params[0]))
                     #total_params = sum(p.numel() for p in model.parameters())
                     #print(total_params)
@@ -63,8 +61,8 @@ for alpha in [0,1e-4]:
             best_param=list_params[np.argmin(scores)]
             print(f"best_param is {best_param}")
             best_param_list.append(best_param)
-            torch.manual_seed(2023)
-            train,val,test,train_mean,train_std=cv_split(perms,cases,ranks,repeat,test_split,3,batch_size=64)
+            torch.manual_seed(seed)
+            train,val,test,train_mean,train_std=cv_split(perms,cases,ranks,repeat,test_split,3,batch_size=72)
             model=LPSC_LSTM(closure_input_sizes,latent_size=int(best_param[2]),mlp_size=int(best_param[1]),num_hidden_layer=int(best_param[0]))
             train_h,val_h,test_h=train_model(model,alpha,beta,train,val,test,epochs=100,lr=2*1e-3,device=device)
             model.turn_on_closure()
