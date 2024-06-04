05207e1bef1f67c53d6d246a2f8afe07fb3096ca
bobjz <43043287+bobjz@users.noreply.github.com>
2024-06-03 20:47:23 -0700

    Update LSTM.py

--- synthetic_experiments/LSTM.py
+++ synthetic_experiments/LSTM.py
@@ -36,7 +36,7 @@ for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
     rmse=[]
     er=[]
     best_param_list=[]
-    for repeat in range(2):
+    for repeat in range(3):
         for test_split in range(5):
             num=[2,3]
             hidden_dim=[8,16]
