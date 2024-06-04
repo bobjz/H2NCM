8abd23ba591fd07b0e9b13422395bddbe8b01c98
bobjz <43043287+bobjz@users.noreply.github.com>
2024-06-03 21:09:31 -0700

    Update UVA_full.py

--- UVA_full.py
+++ UVA_full.py
@@ -36,19 +36,20 @@ for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
     rmse=[]
     er=[]
     best_param_list=[]
-    for repeat in range(1):
+    for repeat in range(1): #change to 3 for alpha=1
         for test_split in range(6):
-            torch.manual_seed(2023)
-            train,val,test,train_mean,train_std=cv_split2(perms,cases,ranks,repeat,test_split,3,batch_size=72,train_intervention="insulin_carb",test_intervention="ins_carb_ratio")
+            seed=2023+repeat
+            torch.manual_seed(seed)
+            train,val,test,train_mean,train_std=cv_split2(perms,cases,ranks,repeat,test_split,3,batch_size=72)
             model=UVA_LSTM()
             train_h,val_h,test_h=train_model(model,alpha,beta,train,val,test,epochs=150,lr=1*1e-2,\
-                                             device=device,path=f"UVAOOD_full_{alpha}_{repeat}_{test_split}.pth")
+                                             device=device,path=f"UVA_full_{alpha}_{repeat}_{test_split}.pth")
             print(f"repeat {repeat} test_split {test_split} pred{train_std[0]*np.sqrt(test_h[np.argmin(val_h)][0])} causal{test_h[np.argmin(val_h)][1]}")
             rmse.append(train_std[0]*np.sqrt(test_h[np.argmin(val_h)][0]))
             er.append(test_h[np.argmin(val_h)][1])
 
-    np.save(f"UVAOOD_full_a{alpha}_pred.npy",rmse)
-    np.save(f"UVAOOD_full_a{alpha}_causal.npy",er)
-    np.save(f"UVAOOD_full_a{alpha}_best_params.npy",best_param_list)
-    print(f"UVAOOD_full_{alpha} RMSE {np.mean(rmse)}")
-    print(f"UVAOOD_full_{alpha} Classification Error Rate {np.sort(er)}")
+    np.save(f"UVA_full_a{alpha}_pred.npy",rmse)
+    np.save(f"UVA_full_a{alpha}_causal.npy",er)
+    np.save(f"UVA_full_a{alpha}_best_params.npy",best_param_list)
+    print(f"UVA_full_{alpha} RMSE {np.mean(rmse)}")
+    print(f"UVA_full_{alpha} Classification Error Rate {np.sort(er)}")
