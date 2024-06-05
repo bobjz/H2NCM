6a7627e38e0aaab52c08a5d30cd5b8f9b140c363
bobjz <43043287+bobjz@users.noreply.github.com>
2024-06-05 13:01:46 -0700

    Update MNODE.py

--- MNODE.py
+++ MNODE.py
@@ -37,13 +37,14 @@ dag=[[[0,3,4,5,6],[],[5]],\
      [[4],[2,3],[3]],\
      [[4,5],[],[2]],\
      [[6],[1],[2]]]
+mod_name="MNODE"
 beta=1e4
 #tune hyperparam
 for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
     rmse=[]
     er=[]
     best_param_list=[]
-    for repeat in range(1): #change to 3 for alpha=1
+    for repeat in range(3): 
         for test_split in range(6):
             seed=2023+repeat
             num_hidden_layers=[2,3]
@@ -73,13 +74,13 @@ for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
             model=MNODE_LSTM(DAG=dag,latent_size=len(dag),output_ind=0,\
                              mlp_size=int(best_param[0]),num_hidden_layers=int(best_param[1]),dropout=best_param[2])
             train_h,val_h,test_h=train_model(model,alpha,beta,train,val,test,epochs=100,lr=2*1e-3,\
-                                             device=device,path=f"MNODE_{alpha}_{repeat}_{test_split}.pth")
+                                             device=device,path=f"{mod_name}_{alpha}_{repeat}_{test_split}.pth")
             print(f"repeat {repeat} test_split {test_split} pred{train_std[0]*np.sqrt(test_h[np.argmin(val_h)][0])} causal{test_h[np.argmin(val_h)][1]}")
             rmse.append(train_std[0]*np.sqrt(test_h[np.argmin(val_h)][0]))
             er.append(test_h[np.argmin(val_h)][1])
 
-    np.save(f"MNODE_a{alpha}_pred.npy",rmse)
-    np.save(f"MNODE_a{alpha}_causal.npy",er)
-    np.save(f"MNODE_a{alpha}_best_params.npy",best_param_list)
-    print(f"MNODE_{alpha} RMSE {np.mean(rmse)}")
-    print(f"MNODE_{alpha} Classification Error Rate {np.sort(er)}")
+    np.save(f"{mod_name}_a{alpha}_pred.npy",rmse)
+    np.save(f"{mod_name}_a{alpha}_causal.npy",er)
+    np.save(f"{mod_name}_a{alpha}_best_params.npy",best_param_list)
+    print(f"{mod_name}_{alpha} RMSE {np.mean(rmse)}")
+    print(f"{mod_name}_{alpha} Classification Error Rate {np.sort(er)}")
