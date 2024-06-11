35077415fa24c988c67e90986374f0f8936250af
bobjz <43043287+bobjz@users.noreply.github.com>
2024-06-10 17:35:35 -0700

    Update LPSC.py

--- insulin-carb_ratio/LPSC.py
+++ insulin-carb_ratio/LPSC.py
@@ -15,14 +15,11 @@ GPU_ID=3
 device = torch.device('cuda:'+str(GPU_ID) if torch.cuda.is_available() else 'cpu')
 print(device)
 
-
 cases=np.load("/dfs/scratch1/bobjz/ICML_paper_data/new_icml_cases.npy")
 ranks=np.load("/dfs/scratch1/bobjz/ICML_paper_data/new_icml_ranks.npy").astype("float64")
 
-
 print(cases.shape)
 print(ranks.shape)
-
 
 repeats=3
 rng=np.random.default_rng(seed=2024)
@@ -30,6 +27,7 @@ perms=np.zeros((repeats,cases.shape[0]),dtype='int32')
 for i in range(repeats):
     perms[i]=rng.permutation(cases.shape[0])
 
+mod_name="LPSC_OOD"
 closure_input_sizes=[2,2,2,3,2,2,2,4,2]
 beta=1e4
 #tune hyperparam
@@ -37,7 +35,7 @@ for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
     rmse=[]
     er=[]
     best_param_list=[]
-    for repeat in range(1): #change to 3 for alpha=1
+    for repeat in range(3):
         for test_split in range(6):
             seed=2023+repeat
             num_hidden_layers=[2,3]
@@ -71,13 +69,13 @@ for alpha in [0,1e-4,1e-3,1e-2,1e-1,1]:
             train_h,val_h,test_h=train_model(model,alpha,beta,train,val,test,epochs=100,lr=2*1e-3,device=device)
             model.turn_on_closure()
             train_h,val_h,test_h=train_model(model,alpha,beta,train,val,test,epochs=50,lr=2*1e-3,\
-                                             device=device,path=f"LPSC_{alpha}_{repeat}_{test_split}.pth")
+                                             device=device,path=f"{mod_name}_{alpha}_{repeat}_{test_split}.pth")
             print(f"repeat {repeat} test_split {test_split} pred{train_std[0]*np.sqrt(test_h[np.argmin(val_h)][0])} causal{test_h[np.argmin(val_h)][1]}")
             rmse.append(train_std[0]*np.sqrt(test_h[np.argmin(val_h)][0]))
             er.append(test_h[np.argmin(val_h)][1])
 
-    np.save(f"LPSC_a{alpha}_pred.npy",rmse)
-    np.save(f"LPSC_a{alpha}_causal.npy",er)
-    np.save(f"LPSC_a{alpha}_best_params.npy",best_param_list)
-    print(f"LPSC_{alpha} RMSE {np.mean(rmse)}")
-    print(f"LPSC_{alpha} Classification Error Rate {np.sort(er)}")
+    np.save(f"{mod_name}_a{alpha}_pred.npy",rmse)
+    np.save(f"{mod_name}_a{alpha}_causal.npy",er)
+    np.save(f"{mod_name}_a{alpha}_best_params.npy",best_param_list)
+    print(f"{mod_name}_{alpha} RMSE {np.mean(rmse)}")
+    print(f"{mod_name}_{alpha} Classification Error Rate {np.sort(er)}")
